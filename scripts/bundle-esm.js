584c82a2283b72f5dfef854409503795a84274c7
Yuya Tanaka <yuya.presto@gmail.com>
2024-07-11 18:37:17 +0900

    Fix bundling

--- scripts/bundle-esm.js
+++ scripts/bundle-esm.js
@@ -1,10 +1,12 @@
 const esbuild = require('esbuild');
-const { nodeExternalsPlugin } = require('esbuild-node-externals');
 
 esbuild.build({
   entryPoints: ['node_modules/open/index.js'],
   bundle: true,
   platform: 'node',
   outfile: 'built-esm/open.js',
-  plugins: [nodeExternalsPlugin()],
+  define: {
+    "import.meta.url": 'import_meta_url'
+  },
+  inject: ['scripts/importMetaUrlShim.js'],
 });
---
+++ scripts/importMetaUrlShim.js
@@ -0,0 +1,2 @@
+// https://zenn.dev/sosukesuzuki/articles/44992fc109ddb2
+export const import_meta_url = require("url").pathToFileURL(__filename);
